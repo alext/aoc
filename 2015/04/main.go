858d1d6e81fe811215476ab196fc53bfa03c6ffd
Alex Tomlins <alex@tomlins.org.uk>
2016-12-03 19:11:02 +0000

    2015 Day 4 part 2

--- 2015/04/main.go
+++ 2015/04/main.go
@@ -12,7 +12,7 @@ func main() {
 	for i := 1; ; i++ {
 		input := secret + strconv.Itoa(i)
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(input)))
-		if hash[0:5] == "00000" {
+		if hash[0:6] == "000000" {
 			fmt.Println("Found:", i)
 			break
 		}

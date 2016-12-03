6540f45d1e247e648384d552515610cfa8a448a4
Alex Tomlins <alex@tomlins.org.uk>
2016-12-03 17:52:28 +0000

    2016 Day 3 part 2

--- 2016/03/main.go
+++ 2016/03/main.go
@@ -24,15 +24,26 @@ func main() {
 
 	possibleCount := 0
 
+	var points [3][3]int
+	index := 0
+
 	for scanner.Scan() {
-		t := Triangle{0, 0, 0}
-		_, err := fmt.Sscan(scanner.Text(), &t[0], &t[1], &t[2])
+		_, err := fmt.Sscan(scanner.Text(), &points[index][0], &points[index][1], &points[index][2])
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(t)
-		if t.Possible() {
-			possibleCount += 1
+		index += 1
+		if index == 3 {
+			for i := 0; i < 3; i++ {
+				t := Triangle{points[0][i], points[1][i], points[2][i]}
+				fmt.Println(t)
+				if t.Possible() {
+					possibleCount += 1
+				} else {
+					fmt.Println("  impossible")
+				}
+			}
+			index = 0
 		}
 	}
 	if err := scanner.Err(); err != nil {

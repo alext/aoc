32ff3d54f798ce92718b9a6bb6f22ac6ff0e4c4f
Alex Tomlins <alex@tomlins.org.uk>
2016-12-03 18:14:51 +0000

    2015 Day 2 part 2

--- 2015/02/main.go
+++ 2015/02/main.go
@@ -10,9 +10,18 @@ import (
 
 type Present []int
 
-func (p Present) Area() int {
+func NewPresent(w, h, l int) Present {
+	p := Present{w, h, l}
 	sort.Ints(p)
+	return p
+}
+
+func (p Present) Area() int {
 	return 3*p[0]*p[1] + 2*p[1]*p[2] + 2*p[0]*p[2]
+}
+
+func (p Present) Ribon() int {
+	return 2*p[0] + 2*p[1] + p[0]*p[1]*p[2]
 }
 
 func (p Present) String() string {
@@ -23,6 +32,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	totalArea := 0
+	totalRibbon := 0
 
 	for scanner.Scan() {
 		var dims [3]int
@@ -30,11 +40,13 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		p := Present{dims[0], dims[1], dims[2]}
+		p := NewPresent(dims[0], dims[1], dims[2])
 		totalArea += p.Area()
+		totalRibbon += p.Ribon()
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Total area:", totalArea)
+	fmt.Println("Total ribon:", totalRibbon)
 }

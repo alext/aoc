d8a1d8bee3298443d8d536f86c8526e17f59cbb5
Alex Tomlins <alex@tomlins.org.uk>
2016-12-03 19:09:33 +0000

    2015 Day 4 part 1

---
+++ 2015/04/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+const secret = "iwrupvqb"
+
+func main() {
+	for i := 1; ; i++ {
+		input := secret + strconv.Itoa(i)
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(input)))
+		if hash[0:5] == "00000" {
+			fmt.Println("Found:", i)
+			break
+		}
+	}
+}

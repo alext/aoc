ebfc35afd52dc8bd78002e47b162425bc0853e51
Alex Tomlins <alex@tomlins.org.uk>
2016-12-05 21:55:56 +0000

    2016 Day 5 part 2

--- 2016/05/main.go
+++ 2016/05/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"crypto/md5"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 const doorID = "abbhdwsy"
 
 func main() {
-	var code bytes.Buffer
+	code := make([]byte, 8)
+	for i := 0; i < len(code); i++ {
+		code[i] = '_'
+	}
+	fmt.Print("Code: ", string(code))
 
-	foundChars := 0
 	for i := 1; ; i++ {
 		input := doorID + strconv.Itoa(i)
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(input)))
 		if hash[0:5] == "00000" {
-			code.WriteByte(hash[5])
-			foundChars += 1
-			if foundChars >= 8 {
-				break
+			if '0' <= hash[5] && hash[5] <= '7' {
+				pos := hash[5] - '0'
+				if code[pos] == '_' {
+					code[pos] = hash[6]
+					fmt.Print("\rCode: ", string(code))
+				}
+				if !strings.Contains(string(code), "_") {
+					break
+				}
 			}
 		}
 	}
-	fmt.Println("Code:", code.String())
+	fmt.Println("")
 }

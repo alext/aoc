5d41d1687ccfcc12466ea6a1d8fdd91b1e21e77e
Alex Tomlins <alex@tomlins.org.uk>
2016-12-05 22:15:01 +0000

    2015 Day 6 part2

--- 2015/06/main.go
+++ 2015/06/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/alext/aoc/helpers"
 )
 
-var lights [1000][1000]bool
+var lights [1000][1000]int
 
 var instrRe = regexp.MustCompile(`(turn (?:on|off)|toggle)\s+(\d+),(\d+)\s+through\s+(\d+),(\d+)`)
 
@@ -29,23 +29,23 @@ func main() {
 			for j := y1; j <= y2; j++ {
 				switch matches[1] {
 				case "turn on":
-					lights[i][j] = true
+					lights[i][j] += 1
 				case "turn off":
-					lights[i][j] = false
+					if lights[i][j] > 0 {
+						lights[i][j] -= 1
+					}
 				case "toggle":
-					lights[i][j] = !lights[i][j]
+					lights[i][j] += 2
 				}
 			}
 		}
 	})
 
-	onCount := 0
+	totalBrightness := 0
 	for i := 0; i < len(lights); i++ {
 		for j := 0; j < len(lights[0]); j++ {
-			if lights[i][j] {
-				onCount++
-			}
+			totalBrightness += lights[i][j]
 		}
 	}
-	fmt.Println("On count:", onCount)
+	fmt.Println("Total Brightness:", totalBrightness)
 }

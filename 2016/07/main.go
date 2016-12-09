48f1c9a7ee8e9bc54eff1a64240ca065b3a8ff7f
Alex Tomlins <alex@tomlins.org.uk>
2016-12-09 14:01:13 +0000

    2016 day 7 part 2

--- 2016/07/main.go
+++ 2016/07/main.go
@@ -48,6 +48,26 @@ func (i *IPv7) SupportsTLS() bool {
 	return false
 }
 
+func (i *IPv7) SupportsSSL() bool {
+	babs := make([]string, 0)
+	for _, s := range i.addressSegments {
+		for _, bab := range getBABs(s) {
+			babs = append(babs, bab)
+		}
+	}
+	if len(babs) == 0 {
+		return false
+	}
+	for _, s := range i.hypernetSequences {
+		for _, bab := range babs {
+			if strings.Contains(s, bab) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func containsABBA(in string) bool {
 	window := make([]rune, 0)
 	for _, r := range in {
@@ -65,13 +85,36 @@ func containsABBA(in string) bool {
 	return false
 }
 
+func getBABs(in string) []string {
+	babs := make([]string, 0)
+	window := make([]rune, 0)
+	for _, r := range in {
+		if len(window) >= 3 {
+			window = window[1:3]
+		}
+		window = append(window, r)
+		if len(window) < 3 {
+			continue
+		}
+		if window[0] == window[2] && window[0] != window[1] {
+			babs = append(babs, fmt.Sprintf("%c%c%c", window[1], window[0], window[1]))
+		}
+	}
+	return babs
+}
+
 func main() {
 	tlsAddresses := 0
+	sslAddresses := 0
 	helpers.ScanLines(os.Stdin, func(line string) {
 		addr := Parse(strings.NewReader(line))
 		if addr.SupportsTLS() {
 			tlsAddresses += 1
 		}
+		if addr.SupportsSSL() {
+			sslAddresses += 1
+		}
 	})
 	fmt.Println("TLS supporting addresses:", tlsAddresses)
+	fmt.Println("SSL supporting addresses:", sslAddresses)
 }

a48b033b3e2427a14fe62f24e540891cfd25eb87
Alex Tomlins <alex@tomlins.org.uk>
2016-12-23 10:23:49 +0000

    2016 Day 14 part 1

---
+++ 2016/14/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+
+	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+)
+
+const salt = "ahsbgdzn"
+
+type Candidate struct {
+	Hash  string
+	Digit string
+	Index int
+}
+
+func (c Candidate) String() string {
+	return fmt.Sprintf("%s triple: %s index: %d", c.Hash, c.Digit, c.Index)
+}
+
+func main() {
+	keys := make([]*Candidate, 0)
+	candidates := make(map[string][]*Candidate)
+
+	var (
+		letterx3 = pcre.MustCompile(`(.)\1{2}`, 0)
+		letterx5 = pcre.MustCompile(`(.)\1{4}`, 0)
+	)
+	for i := 0; ; i++ {
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(salt+strconv.Itoa(i))))
+		matcher := letterx3.MatcherString(hash, 0)
+		if !matcher.Matches() {
+			continue
+		}
+
+		candidate := &Candidate{
+			Hash:  hash,
+			Digit: matcher.GroupString(1),
+			Index: i,
+		}
+
+		matcher = letterx5.MatcherString(hash, 0)
+		if matcher.Matches() {
+			//fmt.Printf("Found 5x in: %s (index %d)\n", hash, i)
+			digit := matcher.GroupString(1)
+			for _, c := range candidates[digit] {
+				//fmt.Println("  Considering", c)
+				if i-c.Index <= 1000 {
+					keys = append(keys, c)
+				}
+			}
+			delete(candidates, digit)
+			//fmt.Println("keys so far:", len(keys))
+		}
+
+		// triple and 5x in same hash doesn't count so has to be appended afterwards
+		candidates[candidate.Digit] = append(candidates[candidate.Digit], candidate)
+
+		if len(keys) >= 64 {
+			fmt.Println("Found 64th key at index", keys[63].Index)
+			break
+		}
+	}
+}

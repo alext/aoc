c19539f8b49e86c870308d45a2a41e8de62d707b
Alex Tomlins <alex@tomlins.org.uk>
2017-01-19 12:58:20 +0000

    2016 Day 15 part 1

---
+++ 2016/15/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/alext/aoc/helpers"
+)
+
+type Disc struct {
+	Index     int
+	Positions int
+	Start     int
+}
+
+func (d Disc) AdjustedPosition(time int) int {
+	return (d.Start + time + d.Index) % d.Positions
+}
+
+func discsAligned(discs []*Disc, time int) bool {
+	for _, d := range discs {
+		if d.AdjustedPosition(time) != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var discs []*Disc
+
+	helpers.ScanLines(os.Stdin, func(line string) {
+		d := &Disc{}
+		_, err := fmt.Sscanf(line, "Disc #%d has %d positions; at time=0, it is at position %d.", &d.Index, &d.Positions, &d.Start)
+		if err != nil {
+			log.Fatal("Failed to match input line " + line)
+		}
+		discs = append(discs, d)
+	})
+
+	for time := 0; true; time++ {
+		if discsAligned(discs, time) {
+			fmt.Println("Discs alligned at time:", time)
+			break
+		}
+	}
+}

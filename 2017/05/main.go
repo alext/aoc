504ca7635a13ed7c5e5aa395eaa298aecc7f4956
Alex Tomlins <alex@tomlins.org.uk>
2017-12-20 16:33:51 +0000

    2017-05 part 1

---
+++ 2017/05/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alext/aoc/helpers"
+)
+
+type Program struct {
+	Instructions []int
+	PC           int
+}
+
+func (p *Program) Step() {
+	nextPC := p.PC + p.Instructions[p.PC]
+	p.Instructions[p.PC]++
+	p.PC = nextPC
+}
+
+func (p *Program) Escaped() bool {
+	return p.PC < 0 || p.PC >= len(p.Instructions)
+}
+
+func main() {
+	p := &Program{}
+	helpers.ScanLines(os.Stdin, func(line string) {
+		p.Instructions = append(p.Instructions, helpers.MustAtoi(line))
+	})
+
+	for count := 1; true; count++ {
+		p.Step()
+		if p.Escaped() {
+			fmt.Printf("Escaped in %d steps\n", count)
+			break
+		}
+	}
+}

3c11d90891834a4e0b59393541c6235e2081c850
Alex Tomlins <alex@tomlins.org.uk>
2018-11-16 14:06:45 +0000

    2017-10 part 2

--- 2017/10/main.go
+++ 2017/10/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/alext/aoc/helpers"
 )
 
 const (
-	listSize = 256
-	input    = "106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36"
+	listSize  = 256
+	blockSize = 16
+	input     = "106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36"
+	rounds    = 64
 )
 
-//const (
-//listSize = 5
-//input    = "3, 4, 1, 5"
-//)
+var extraLengths = []int{17, 31, 73, 47, 23}
 
 type circularSlice []int
 
@@ -30,23 +30,68 @@ func (c circularSlice) reverseSection(pos, length int) {
 	}
 }
 
+type knotHash struct {
+	list circularSlice
+	pos  int
+	skip int
+}
+
+func New(size int) *knotHash {
+	h := &knotHash{
+		list: make(circularSlice, size),
+	}
+	for i := 0; i < size; i++ {
+		h.list[i] = i
+	}
+	return h
+}
+
+func (h *knotHash) runRound(lengths []int) {
+	for _, length := range lengths {
+		h.list.reverseSection(h.pos, length)
+		h.pos += (length + h.skip) % len(h.list)
+		h.skip++
+	}
+}
+
+func (h *knotHash) denseHash() []int {
+	blocks := len(h.list) / blockSize
+	result := make([]int, blocks)
+	for b := 0; b < blocks; b++ {
+		for i := b * blockSize; i < (b+1)*blockSize; i++ {
+			result[b] = result[b] ^ h.list[i]
+		}
+	}
+	return result
+}
+
+func hexString(input []int) string {
+	var out strings.Builder
+	for _, i := range input {
+		fmt.Fprintf(&out, "%02x", i)
+	}
+	return out.String()
+}
+
 func main() {
 	lengths := make([]int, 0)
 	for _, s := range helpers.SplitCSV(input) {
 		lengths = append(lengths, helpers.MustAtoi(s))
 	}
 
-	list := make(circularSlice, listSize)
-	for i := 0; i < listSize; i++ {
-		list[i] = i
-	}
+	hash := New(listSize)
+	hash.runRound(lengths)
+	fmt.Printf("First 2 numbers: %d, %d. Product: %d\n", hash.list[0], hash.list[1], hash.list[0]*hash.list[1])
 
-	pos, skip := 0, 0
-	for _, length := range lengths {
-		list.reverseSection(pos, length)
-		pos += (length + skip) % listSize
-		skip++
+	lengths2 := make([]int, 0)
+	helpers.ScanRunes(strings.NewReader(input), func(r string) {
+		lengths2 = append(lengths2, int(r[0]))
+	})
+	lengths2 = append(lengths2, extraLengths...)
+	hash2 := New(listSize)
+	for i := 0; i < rounds; i++ {
+		hash2.runRound(lengths2)
 	}
-
-	fmt.Printf("First 2 numbers: %d, %d. Product: %d\n", list[0], list[1], list[0]*list[1])
+	fmt.Printf("First 2 numbers: %d, %d. Product: %d\n", hash2.list[0], hash2.list[1], hash2.list[0]*hash2.list[1])
+	fmt.Println(hexString(hash2.denseHash()))
 }

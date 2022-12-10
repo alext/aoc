d188be99ddbac03de09aef30e921cd8788e2006c
Alex Tomlins <alex@tomlins.org.uk>
2022-12-10 10:48:21 +0000

    2022-09 part 2

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -14,22 +14,27 @@ type Pos struct {
 	Y int
 }
 
+func (p Pos) String() string {
+	return fmt.Sprintf("(%d,%d)", p.X, p.Y)
+}
+
 type Rope struct {
 	Head        Pos
-	Tail        Pos
+	Tails       []Pos
 	TailVisited map[Pos]bool
 }
 
-func NewRope() *Rope {
+func NewRope(numTails int) *Rope {
 	r := &Rope{
+		Tails:       make([]Pos, numTails),
 		TailVisited: make(map[Pos]bool),
 	}
-	r.TailVisited[r.Tail] = true
+	r.TailVisited[Pos{0, 0}] = true
 	return r
 }
 
 func (r Rope) String() string {
-	return fmt.Sprintf("Head (%d,%d) Tail (%d,%d)", r.Head.X, r.Head.Y, r.Tail.X, r.Tail.Y)
+	return fmt.Sprintf("Head %s Tail %s", r.Head, r.Tails)
 }
 func absSign(n int) (int, int) {
 	if n >= 0 {
@@ -38,28 +43,44 @@ func absSign(n int) (int, int) {
 	return -n, -1
 }
 
-func (r *Rope) moveTail() {
-	xDelta, xSign := absSign(r.Head.X - r.Tail.X)
-	yDelta, ySign := absSign(r.Head.Y - r.Tail.Y)
+func (r *Rope) moveTail(index int) bool {
+	var current, previous *Pos
+	current = &r.Tails[index]
+	if index == 0 {
+		previous = &r.Head
+	} else {
+		previous = &r.Tails[index-1]
+	}
 
+	xDelta, xSign := absSign(previous.X - current.X)
+	yDelta, ySign := absSign(previous.Y - current.Y)
 	if xDelta < 2 && yDelta < 2 {
 		// No move needed
-		return
+		return false
 	}
 
 	if xDelta >= 2 {
-		r.Tail.X += xSign
+		current.X += xSign
 		if yDelta > 0 {
-			r.Tail.Y += ySign
+			current.Y += ySign
 		}
-	}
-	if yDelta >= 2 {
-		r.Tail.Y += ySign
+	} else if yDelta >= 2 {
+		current.Y += ySign
 		if xDelta > 0 {
-			r.Tail.X += xSign
+			current.X += xSign
 		}
 	}
-	r.TailVisited[r.Tail] = true
+	return true
+}
+
+func (r *Rope) moveTails() {
+	for i := range r.Tails {
+		moved := r.moveTail(i)
+		if !moved {
+			return
+		}
+	}
+	r.TailVisited[r.Tails[len(r.Tails)-1]] = true
 }
 
 func (r *Rope) moveOne(direction string) {
@@ -75,7 +96,7 @@ func (r *Rope) moveOne(direction string) {
 	default:
 		log.Fatalln("Unexpected direction:", direction)
 	}
-	r.moveTail()
+	r.moveTails()
 }
 
 func (r *Rope) Move(direction string, amount int) {
@@ -85,13 +106,16 @@ func (r *Rope) Move(direction string, amount int) {
 }
 
 func main() {
-	r := NewRope()
-	fmt.Println("Starting:", r)
+	r := NewRope(1)
+	r2 := NewRope(9)
 
 	helpers.ScanLines(os.Stdin, func(line string) {
 		dir, amount, _ := strings.Cut(line, " ")
 		r.Move(dir, helpers.MustAtoi(amount))
+		r2.Move(dir, helpers.MustAtoi(amount))
 	})
 
-	fmt.Println("Visited squares:", len(r.TailVisited))
+	fmt.Println("Visited squares r :", len(r.TailVisited))
+	fmt.Println("Visited squares r2:", len(r2.TailVisited))
+
 }

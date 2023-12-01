806a02de7ef688ecf70ef6ffdcee9b2363ff79ec
Alex Tomlins <alex@tomlins.org.uk>
2023-12-01 22:16:02 +0000

    2023-01 part 2

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -9,26 +9,54 @@ import (
 	"github.com/alext/aoc/helpers"
 )
 
+var numbers = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func parseNumber(input string) int {
+	if len(input) == 1 {
+		return helpers.MustAtoi(input)
+	}
+	value, ok := numbers[input]
+	if !ok {
+		log.Fatal("Failed to parse input", input)
+	}
+	return value
+}
+
 func main() {
 
 	total := 0
 
-	firstRe := regexp.MustCompile(`^[^\d]*(\d)`)
-	lastRe := regexp.MustCompile(`(\d)[^\d]*$`)
+	digits := `\d`
+	for word := range numbers {
+		digits += `|` + word
+	}
+	firstRe := regexp.MustCompile(`^.*?(` + digits + `)`)
+	lastRe := regexp.MustCompile(`.*(` + digits + `).*?$`)
 
 	helpers.ScanLines(os.Stdin, func(line string) {
 		matches := firstRe.FindStringSubmatch(line)
 		if matches == nil {
 			log.Fatal("Failed to match first digit in line", line)
 		}
-		first := matches[1]
+		first := parseNumber(matches[1])
 
 		matches = lastRe.FindStringSubmatch(line)
 		if matches == nil {
 			log.Fatal("Failed to match last digit in line", line)
 		}
+		last := parseNumber(matches[1])
 
-		number := helpers.MustAtoi(first + matches[1])
+		number := first*10 + last
 		total += number
 	})
 

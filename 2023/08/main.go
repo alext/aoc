c812f076382b7890343bd136d4698e1fb26572cf
Alex Tomlins <alex@tomlins.org.uk>
2023-12-09 09:13:29 +0000

    2023-08 part 2 - non-working
    works for small examples, but fails to complete in a reasonable amount
    of time for the real example.  Need a different approach

--- 2023/08/main.go
+++ 2023/08/main.go
@@ -14,9 +14,11 @@ type Node struct {
 	Right *Node
 }
 
-func buildGraph(inCh <-chan string) *Node {
-	var startNode *Node
+func (n Node) String() string {
+	return fmt.Sprintf("%s (%s, %s)", n.Label, n.Left.Label, n.Right.Label)
+}
 
+func buildGraph(inCh <-chan string) map[string]*Node {
 	nodes := make(map[string]*Node)
 	getNode := func(label string) *Node {
 		node, found := nodes[label]
@@ -35,24 +37,17 @@ func buildGraph(inCh <-chan string) *Node {
 		node := getNode(label)
 		node.Left = getNode(left)
 		node.Right = getNode(right)
-		if node.Label == "AAA" {
-			startNode = node
-		}
 	}
 
-	return startNode
+	return nodes
 }
-func main() {
-	inCh := helpers.StreamLines(os.Stdin)
 
-	instructions := <-inCh
-	<-inCh // Discard blank line
-
-	start := buildGraph(inCh)
-
-	current := start
+func moveCount(graph map[string]*Node, instructions string) int {
+	current, ok := graph["AAA"]
+	if !ok {
+		return -1
+	}
 	moveCount := 0
-outer:
 	for {
 		for _, i := range instructions {
 			switch i {
@@ -65,9 +60,57 @@ outer:
 			}
 			moveCount++
 			if current.Label == "ZZZ" {
-				break outer
+				return moveCount
 			}
 		}
 	}
-	fmt.Println("Total moves:", moveCount)
+}
+
+func moveCountWithGhosts(graph map[string]*Node, instructions string) int {
+	var currentSet []*Node
+	for label, node := range graph {
+		if label[2] == 'A' {
+			currentSet = append(currentSet, node)
+		}
+	}
+	fmt.Println(currentSet)
+	if len(currentSet) == 0 {
+		return -1
+	}
+
+	moveCount := 0
+	for {
+		for _, inst := range instructions {
+			allZ := true
+			moveCount++
+			for i := range currentSet {
+				switch inst {
+				case 'L':
+					currentSet[i] = currentSet[i].Left
+				case 'R':
+					currentSet[i] = currentSet[i].Right
+				default:
+					log.Fatalln("Unexpected instruction:", inst)
+				}
+				if currentSet[i].Label[2] != 'Z' {
+					allZ = false
+				}
+			}
+			if allZ {
+				return moveCount
+			}
+		}
+	}
+}
+
+func main() {
+	inCh := helpers.StreamLines(os.Stdin)
+
+	instructions := <-inCh
+	<-inCh // Discard blank line
+
+	graph := buildGraph(inCh)
+
+	fmt.Println("Total moves:", moveCount(graph, instructions))
+	fmt.Println("Total moves with ghosts:", moveCountWithGhosts(graph, instructions))
 }

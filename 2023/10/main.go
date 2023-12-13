fa5bcde6ff4926cb2be3fd997ff1591172f3e100
Alex Tomlins <alex@tomlins.org.uk>
2023-12-13 16:51:10 +0000

    2023-10 part 2

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type Pipe struct {
+	ch    string
 	Row   int
 	Col   int
 	North bool
@@ -23,7 +24,7 @@ type Pipe struct {
 }
 
 func ParsePipe(ch string, row, col int) *Pipe {
-	p := &Pipe{Row: row, Col: col}
+	p := &Pipe{ch: ch, Row: row, Col: col}
 	switch ch {
 	case `|`:
 		p.North = true
@@ -58,6 +59,23 @@ type Grid struct {
 	Start *Pipe
 }
 
+func (g Grid) String() string {
+	var s strings.Builder
+	for _, row := range g.Pipes {
+		for _, p := range row {
+			if p == nil {
+				s.WriteString(`.`)
+			} else if p.Start {
+				s.WriteString("S")
+			} else {
+				s.WriteString(p.ch)
+			}
+		}
+		s.WriteString("\n")
+	}
+	return s.String()
+}
+
 func (g Grid) PipeAt(row, col int) *Pipe {
 	if row < 0 || row >= len(g.Pipes) {
 		return nil
@@ -72,15 +90,33 @@ func (g Grid) getStartNeighbours() []*Pipe {
 	var neighbours []*Pipe
 	if p := g.PipeAt(g.Start.Row-1, g.Start.Col); p != nil && p.South {
 		neighbours = append(neighbours, p)
+		g.Start.North = true
 	}
 	if p := g.PipeAt(g.Start.Row+1, g.Start.Col); p != nil && p.North {
 		neighbours = append(neighbours, p)
+		g.Start.South = true
 	}
 	if p := g.PipeAt(g.Start.Row, g.Start.Col-1); p != nil && p.East {
 		neighbours = append(neighbours, p)
+		g.Start.West = true
 	}
 	if p := g.PipeAt(g.Start.Row, g.Start.Col+1); p != nil && p.West {
 		neighbours = append(neighbours, p)
+		g.Start.East = true
+	}
+	switch {
+	case g.Start.North && g.Start.South:
+		g.Start.ch = "|"
+	case g.Start.East && g.Start.West:
+		g.Start.ch = "-"
+	case g.Start.North && g.Start.East:
+		g.Start.ch = "L"
+	case g.Start.North && g.Start.West:
+		g.Start.ch = "J"
+	case g.Start.South && g.Start.East:
+		g.Start.ch = "F"
+	case g.Start.South && g.Start.West:
+		g.Start.ch = "7"
 	}
 
 	if len(neighbours) != 2 {
@@ -148,6 +184,81 @@ func (g Grid) MaxDistance() int {
 	return maxDistance
 }
 
+func (g Grid) ClearNonLoopPipe() {
+	for r, row := range g.Pipes {
+		for c, p := range row {
+			if p == nil {
+				continue
+			}
+			if p.Start || p.Visited {
+				continue
+			}
+			g.Pipes[r][c] = nil
+		}
+	}
+}
+
+func (g Grid) IsInsideLoop(row, col int) bool {
+	if g.Pipes[row][col] != nil {
+		// On the loop - therefore not inside
+		return false
+	}
+	gridRow := g.Pipes[row]
+	crossings := 0
+	for i := col - 1; i >= 0; i-- {
+		p := gridRow[i]
+		if p == nil {
+			continue
+		}
+		segmentStart := ""
+		switch p.ch {
+		case "|":
+			crossings++
+		case "J", "7":
+			segmentStart = p.ch
+		default:
+			log.Fatalln("Open-ended pipe:", p.ch)
+		}
+		for segmentStart != "" {
+			// We're dealing with a horizontal pipe segment. Loop for the start of it.
+			i--
+			p = gridRow[i]
+			if p == nil {
+				log.Fatalf("Dangling segment at r:%d, c:%d from row:%d, col:%d", row, i, row, col)
+			}
+			switch p.ch {
+			case "-":
+				continue
+			case "F":
+				if segmentStart == "J" {
+					crossings++
+				}
+				segmentStart = ""
+			case "L":
+				if segmentStart == "7" {
+					crossings++
+				}
+				segmentStart = ""
+			default:
+				log.Fatalln("Mismatched segment end:", p.ch)
+			}
+		}
+	}
+	return crossings%2 == 1
+}
+
+func (g Grid) CountInsideSquares() int {
+	count := 0
+	for r, row := range g.Pipes {
+		for c := range row {
+			if g.IsInsideLoop(r, c) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
 func main() {
 
 	var g Grid
@@ -166,4 +277,9 @@ func main() {
 	})
 
 	fmt.Println("Max distance:", g.MaxDistance())
+	fmt.Println(g)
+	g.ClearNonLoopPipe()
+	fmt.Println(g)
+
+	fmt.Println("Inside squares:", g.CountInsideSquares())
 }

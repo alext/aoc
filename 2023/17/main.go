0ade03ddb3f096d2805e43c13a67c742b8a924d7
Alex Tomlins <alex@tomlins.org.uk>
2023-12-21 09:44:19 +0000

    2023-17 part 1 - correct but slow

--- 2023/17/main.go
+++ 2023/17/main.go
@@ -37,69 +37,11 @@ func (d Direction) String() string {
 
 type Block struct {
 	HeatLoss int
-	Row      int
-	Col      int
-
-	BestHeatLoss     int
-	EntryDirection   Direction
-	ConsecutiveCount int
-}
-
-func (b Block) Char() string {
-	if b.BestHeatLoss > 0 {
-		return b.EntryDirection.String()
-	}
-	return strconv.Itoa(b.HeatLoss)
+	Pos      helpers.Pos
 }
 
 func (b Block) String() string {
-	return fmt.Sprintf("(%d,%d) %s %d", b.Col, b.Row, b.EntryDirection, b.BestHeatLoss)
-}
-
-type PotentialMove struct {
-	Target    *Block
-	Direction Direction
-}
-
-func (b *Block) NextMoves(g Grid) []*PotentialMove {
-	var nextMoves []*PotentialMove
-	// Going North
-	if b.Row > 0 && b.EntryDirection != South {
-		//if b.EntryDirection != North || b.ConsecutiveCount < 3 {
-		nextMoves = append(nextMoves, &PotentialMove{
-			Target:    g[b.Row-1][b.Col],
-			Direction: North,
-		})
-		//}
-	}
-	// Going East
-	if b.Col < len(g[0])-1 && b.EntryDirection != West {
-		//if b.EntryDirection != East || b.ConsecutiveCount < 3 {
-		nextMoves = append(nextMoves, &PotentialMove{
-			Target:    g[b.Row][b.Col+1],
-			Direction: East,
-		})
-		//}
-	}
-	// Going South
-	if b.Row < len(g)-1 && b.EntryDirection != North {
-		//if b.EntryDirection != South || b.ConsecutiveCount < 3 {
-		nextMoves = append(nextMoves, &PotentialMove{
-			Target:    g[b.Row+1][b.Col],
-			Direction: South,
-		})
-		//}
-	}
-	// Going West
-	if b.Col > 0 && b.EntryDirection != East {
-		//if b.EntryDirection != West || b.ConsecutiveCount < 3 {
-		nextMoves = append(nextMoves, &PotentialMove{
-			Target:    g[b.Row][b.Col-1],
-			Direction: West,
-		})
-		//}
-	}
-	return nextMoves
+	return strconv.Itoa(b.HeatLoss)
 }
 
 type Grid [][]*Block
@@ -108,7 +50,7 @@ func (g Grid) String() string {
 	var b = strings.Builder{}
 	for i, line := range g {
 		for _, block := range line {
-			b.WriteString(block.Char())
+			b.WriteString(block.String())
 		}
 		if i < len(g)-1 {
 			b.WriteString("\n")
@@ -124,8 +66,7 @@ func BuildGrid(input <-chan string) Grid {
 		for i, digit := range strings.Split(line, "") {
 			gridLine = append(gridLine, &Block{
 				HeatLoss: helpers.MustAtoi(digit),
-				Row:      len(grid),
-				Col:      i,
+				Pos:      helpers.Pos{X: i, Y: len(grid)},
 			})
 
 		}
@@ -134,59 +75,177 @@ func BuildGrid(input <-chan string) Grid {
 	return grid
 }
 
-func (g Grid) BestPath() int {
-	start := g[0][0]
-	//start.EntryDirection = West
-	//start.ConsecutiveCount = 3
-	moves := []*Block{start}
+type Move struct {
+	Path           []*Block
+	Target         *Block
+	EntryDirection Direction
 
-	limit := 200
+	DistanceToEnd    int
+	TotalHeatLoss    int
+	ConsecutiveCount int
+	Beaten           bool
+}
+
+func (m *Move) String() string {
+	return fmt.Sprintf("%d%s%s%d", m.TotalHeatLoss, m.Target.Pos, m.EntryDirection, m.ConsecutiveCount)
+}
+
+func (m *Move) PathStr() string {
+	var b strings.Builder
+	for _, blk := range m.Path {
+		b.WriteString(blk.Pos.String())
+		b.WriteString(" ")
+	}
+	b.WriteString(m.Target.Pos.String())
+	return b.String()
+}
+
+func (m *Move) NextMoves(g Grid) []*Move {
+	var nextMoves []*Move
+	b := m.Target
+	nextPath := append(slices.Clone(m.Path), b)
+	// Going North
+	if b.Pos.Y > 0 && m.EntryDirection != South {
+		target := g[b.Pos.Y-1][b.Pos.X]
+		if !slices.Contains(m.Path, target) {
+			nextMoves = append(nextMoves, &Move{
+				Path:           nextPath,
+				Target:         target,
+				EntryDirection: North,
+			})
+		}
+	}
+	// Going East
+	if b.Pos.X < len(g[0])-1 && m.EntryDirection != West {
+		target := g[b.Pos.Y][b.Pos.X+1]
+		if !slices.Contains(m.Path, target) {
+			nextMoves = append(nextMoves, &Move{
+				Path:           nextPath,
+				Target:         target,
+				EntryDirection: East,
+			})
+		}
+	}
+	// Going South
+	if b.Pos.Y < len(g)-1 && m.EntryDirection != North {
+		target := g[b.Pos.Y+1][b.Pos.X]
+		if !slices.Contains(m.Path, target) {
+			nextMoves = append(nextMoves, &Move{
+				Path:           nextPath,
+				Target:         target,
+				EntryDirection: South,
+			})
+		}
+	}
+	// Going West
+	if b.Pos.X > 0 && m.EntryDirection != East {
+		target := g[b.Pos.Y][b.Pos.X-1]
+		if !slices.Contains(m.Path, target) {
+			nextMoves = append(nextMoves, &Move{
+				Path:           nextPath,
+				Target:         target,
+				EntryDirection: West,
+			})
+		}
+	}
+	return nextMoves
+}
+
+// Returns whether m is guaranteed to be better than other based on the move
+// properties alone.
+func (m *Move) BetterThan(other *Move) bool {
+	if m.TotalHeatLoss < other.TotalHeatLoss && m.EntryDirection == other.EntryDirection && m.ConsecutiveCount <= other.ConsecutiveCount {
+		return true
+	}
+	return false
+}
+
+func (g Grid) BestPath() *Move {
+	start := &Move{
+		Target: g[0][0],
+	}
+	endBlock := g[len(g)-1][len(g[0])-1]
+	moves := []*Move{start}
+
+	movesToBlock := make(map[*Block][]*Move)
+
+	limit := 3000
 	for len(moves) > 0 && limit > 0 {
 		move := moves[0]
 		moves = moves[1:]
 
-		if move.Row == len(g)-1 && move.Col == len(g[0])-1 {
-			// We've reached the final square
-			return move.BestHeatLoss
+		if move.Target == endBlock {
+			return move
+			//if best == nil || move.TotalHeatLoss < best.TotalHeatLoss {
+			//    fmt.Printf("Found new best loss=%d, path: %s\n", move.TotalHeatLoss, move.PathStr())
+			//    best = move
+			//    continue
+			//}
 		}
 
-		for _, potentialMove := range move.NextMoves(g) {
-			target := potentialMove.Target
-			if move.EntryDirection == potentialMove.Direction && move.ConsecutiveCount >= 3 {
+		if move.Beaten {
+			continue
+		}
+
+		// best could have improved since this was added to the list
+		//if best != nil && (move.TotalHeatLoss+move.DistanceToEnd) >= best.TotalHeatLoss {
+		//    continue
+		//}
+
+		for _, nextMove := range move.NextMoves(g) {
+			target := nextMove.Target
+			if move.EntryDirection == nextMove.EntryDirection && move.ConsecutiveCount >= 3 {
 				// Can't move more than 3 in a row
 				continue
 			}
-			potentialBest := move.BestHeatLoss + target.HeatLoss
-			if target.BestHeatLoss != 0 && target.BestHeatLoss < potentialBest {
-				// Already a better path to target
+			nextMove.TotalHeatLoss = move.TotalHeatLoss + target.HeatLoss
+			//nextMove.DistanceToEnd = nextMove.Target.Pos.DistanceTo(endBlock.Pos)
+
+			//if best != nil && (nextMove.TotalHeatLoss+move.DistanceToEnd) >= best.TotalHeatLoss {
+			//    continue
+			//}
+
+			if nextMove.EntryDirection == move.EntryDirection {
+				nextMove.ConsecutiveCount = move.ConsecutiveCount + 1
+			} else {
+				nextMove.ConsecutiveCount = 1
+			}
+			for _, otherMove := range movesToBlock[target] {
+				if otherMove.BetterThan(nextMove) {
+					nextMove.Beaten = true
+					break
+				}
+				if nextMove.BetterThan(otherMove) {
+					otherMove.Beaten = true
+					// TODO: remove from movesToBlock
+				}
+			}
+			movesToBlock[target] = slices.DeleteFunc(movesToBlock[target], func(m *Move) bool { return m.Beaten })
+			if nextMove.Beaten {
 				continue
 			}
-			target.BestHeatLoss = potentialBest
-			target.EntryDirection = potentialMove.Direction
-			if potentialMove.Direction == move.EntryDirection {
-				target.ConsecutiveCount = move.ConsecutiveCount + 1
-			} else {
-				target.ConsecutiveCount = 1
-			}
-			if !slices.Contains(moves, target) {
-				moves = append(moves, target)
-			}
+
+			moves = append(moves, nextMove)
+			movesToBlock[target] = append(movesToBlock[target], nextMove)
 		}
 
-		slices.SortFunc(moves, func(a, b *Block) int { return cmp.Compare(a.BestHeatLoss, b.BestHeatLoss) })
+		slices.SortFunc(moves, func(a, b *Move) int { return cmp.Compare(a.TotalHeatLoss, b.TotalHeatLoss) })
 		//fmt.Println("After move:", moves)
 		//limit--
 	}
 
-	return -1
+	//if best != nil {
+	//    return best.TotalHeatLoss
+	//}
+	return nil
 }
 
 func main() {
 	g := BuildGrid(helpers.StreamLines(os.Stdin))
 
-	fmt.Println(g)
+	//fmt.Println(g)
 
-	fmt.Println("Heat loss on best path:", g.BestPath())
-
-	fmt.Println(g)
+	move := g.BestPath()
+	fmt.Println("Best path:", move, move.PathStr())
+	fmt.Println("Heat loss on best path:", move.TotalHeatLoss)
 }
